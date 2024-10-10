ce9603faa8fdc322e297049e88b0370a30e09cdb
Hydraxon91 <57135182+Hydraxon91@users.noreply.github.com>
2024-10-10 18:49:24 +0200

    Adds unimplemented crud methods

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ const port = 3000;
 
 app.use(express.json())
 
+//Read route
+app.get('/todo', (req, res) => {
+    res.json("Needs to be implemented")
+});
+
+//Create route
+app.post('/todo', (req, res) => {
+    res.json("Needs to be implemented")
+});
+
+//Update route
+app.put('/todo/:id', (req, res) => {
+    res.json("Needs to be implemented")
+});
+
+//Delete route
+app.delete('/todo/:id', (req, res) => {
+    res.json("Needs to be implemented")
+});
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
